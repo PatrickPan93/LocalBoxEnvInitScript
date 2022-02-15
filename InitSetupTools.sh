030b40db81c65329f13194393900d7926b630d0b
Patrick Pan <patrick@Patricks-MBP.princeton.epam.com>
2022-02-15 16:30:47 +0800

    update

--- InitSetupTools.sh
+++ InitSetupTools.sh
@@ -6,3 +6,9 @@ curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 rm -f istio-${ISTIO_VERSION}
 wget -c https://sealyun.oss-cn-beijing.aliyuncs.com/latest/sealos && \
     chmod +x sealos && mv sealos /usr/bin 
+
+echo "sealos init --passwd '123456' \
+	--master 192.168.0.2  --master 192.168.0.3  --master 192.168.0.4  \
+	--node 192.168.0.5 \
+	--pkg-url /root/kube1.22.0.tar.gz \
+	--version v1.22.0"
