1cb8df8f457cbf075b255a6353553b125a0d29b6
Patrick Pan <patrick@Patricks-MBP.princeton.epam.com>
2022-02-15 16:18:35 +0800

    update

--- InitSetupTools.sh
+++ InitSetupTools.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.13.0 sh -
+ISTIO_VERSION=1.13.0
+curl -L https://istio.io/downloadIstio | ISTIO_VERSION=${ISTIO_VERSION} sh -
 cp istio-1.13.0/bin/istioctl /usr/local/bin/
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+rm -f istio-${ISTIO_VERSION}
+wget -c https://sealyun.oss-cn-beijing.aliyuncs.com/latest/sealos && \
+    chmod +x sealos && mv sealos /usr/bin 
